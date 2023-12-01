bc82c3bbd1607ce9f770a650920b79112f5c81da
yangux <yeonie05@gmail.com>
2023-12-01 18:16:37 +0900

    refactor: debounced 함수 Promise 반환

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 function debounce(func, wait = 1000, immediate = false) {
   let timeout;
 
-  function debounced(...args) {
-    const callFirst = immediate && !timeout;
-    if (callFirst) func(...args);
+  return function debounced(...args) {
+    return new Promise((resolve, reject) => {
+      const callFirst = immediate && !timeout;
+      if (callFirst) func(...args);
 
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      if (!immediate) {
-        func(...args);
-      }
-    }, wait);
-  }
-
-  return debounced;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        if (!immediate) {
+          func(...args);
+          resolve("[RESOLVED] 원래 함수 실행 완료");
+        } else {
+          reject("[REJECTED] 첫 번째 함수 즉시 실행");
+        }
+      }, wait);
+    });
+  };
 }
 
 function search(text) {
@@ -22,7 +25,15 @@ function search(text) {
 
 const debouncedSearch = debounce(search, undefined, true);
 
-debouncedSearch("zip");
-debouncedSearch("zip");
-debouncedSearch("zip");
-debouncedSearch("zipup");
+debouncedSearch("zip")
+  .then((val) => console.log(val))
+  .catch((err) => console.log(err));
+debouncedSearch("zip")
+  .then((val) => console.log(val))
+  .catch((err) => console.log(err));
+debouncedSearch("zip")
+  .then((val) => console.log(val))
+  .catch((err) => console.log(err));
+debouncedSearch("zipup")
+  .then((val) => console.log(val))
+  .catch((err) => console.log(err));
